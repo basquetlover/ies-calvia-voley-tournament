3e7e36c51ab8bcb54cb6cfe2033ee3f6fbf39eb7
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-10-19 13:43:15 +0200

    first page

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+	  extend: {
+		colors: {
+		  azul: '#0E347D',
+		  amarillo: '#FFC700',
+		  gris: '#383836',
+		  blanco: '#FFFFFF',
+		},
+		screens: {
+		  xs: '220px',  // Agrega un breakpoint para pantallas más pequeñas
+		  sm: '640px',   // Pantallas pequeñas
+		  md: '768px',   // Pantallas medianas
+		  lg: '1024px',  // Pantallas grandes
+		  xl: '1280px',  // Pantallas extra grandes
+		},
+	  },
+	},
+	plugins: [],
+  }
+  
