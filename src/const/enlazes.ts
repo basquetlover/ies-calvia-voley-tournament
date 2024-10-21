757697731103732a0904a6655859151455d9ea7a
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-10-21 18:00:41 +0200

    add footer

---
+++ src/const/enlazes.ts
@@ -0,0 +1,22 @@
+export const ENLAZES = [
+    {
+        href: '/404',
+        nombre: 'Contacto',
+    },
+    {
+        href: '',
+        nombre: 'Equipos',
+    },
+    {
+        href: '',
+        nombre: 'Reglamento',
+    },
+    {
+        href: '',
+        nombre: 'Instalación',
+    },
+    {
+        href: '',
+        nombre: '',
+    }
+]
