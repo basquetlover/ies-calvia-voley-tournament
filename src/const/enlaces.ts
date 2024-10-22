078f5c0d866d57af8f6bf42115039d60c457dee7
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-10-22 11:28:11 +0200

    cambiar nombre archivos

---
+++ src/const/enlaces.ts
@@ -0,0 +1,22 @@
+export const ENLACES = [
+    {
+        href: '',
+        nombre: 'Contacto',
+    },
+    {
+        href: '',
+        nombre: 'Equipos',
+    },
+    {
+        href: '',
+        nombre: 'Reglamento',
+    },
+    {
+        href: '',
+        nombre: 'Instalación',
+    },
+    {
+        href: '',
+        nombre: '',
+    }
+]
