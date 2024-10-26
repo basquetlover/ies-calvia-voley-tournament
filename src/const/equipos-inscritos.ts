395812a181ab6035600c5fb9bb5ceaa90ccab519
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-10-26 17:48:42 +0200

    añadir seccion equipos dinamico

---
+++ src/const/equipos-inscritos.ts
@@ -0,0 +1,24 @@
+export const EQUIPOS_INSCRITOS = [
+    {
+        id: 'team-teto',
+        nombre: 'TEAM Teto',
+        escudo: 'team-teto.png',
+        capitan: 'Miguel Socias',
+        entrenador: 'Pere Alemany',
+        jugadores:[
+            {
+                nombre: 'Miguel Socias',
+                curso: '2n Batch'
+            },
+            {
+                nombre: 'Lucas Hechavarria',
+                curso: '2n Batch'
+            },
+            {
+                nombre: 'Luca Emanuel',
+                curso: '2n Batch'
+            }
+        ]
+  
+    }
+];
