ac9141ec100730c39c301f7c3eb82d3c14cfbdc6
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-11-01 17:35:54 +0100

    añadir tooltip

--- src/const/contacto.ts
+++ src/const/contacto.ts
@@ -1,36 +1,42 @@
 export const CONTACTO = [
     {
         nombre: 'Pere Alemany Tomàs',
+        tooltip: 'Pere',
         cargo: 'Coordinador General',
         email: 'p_alemanyt@a.iescalvia.com',
         descripcion: 'Pere es uno de los encargados de la planificación general del torneo, asegurando que todo funcione de manera fluida. Es de los principal responsable de la logística y la coordinación con los equipos participantes.',
     },
     {
         nombre: 'Miguel Socias Perez',
+        tooltip: 'Miguel',
         cargo: 'Coordinador General',
         email: 'm_sociasp@a.iescalvia.com',
         descripcion: 'Miguel es otro de los encargados de la planificación general del torneo, asegurando que todo funcione de manera fluida. Es de los principal responsable de la logística y la coordinación con los equipos participantes.',
     },
     {
         nombre: 'Lucas Hechavarria Escartin',
+        tooltip: 'Lucas',
         cargo: 'Responsable de Inscripciones',
         email: 'l_hechavarriae@a.iescalvia.com',
         descripcion: 'Lucas gestiona todas las inscripciones de los equipos. Si tienes dudas sobre el registro o el proceso de participación, él es la persona adecuada.',
     },
     {
         nombre: 'Santino Bonavera',
+        tooltip: 'Santino',
         cargo: 'Responsable de Voluntarios',
         email: 's_bonavera@a.iescalvia.com',
         descripcion: 'Santi coordina al equipo de voluntarios que ayuda en el desarrollo del torneo. Si estás interesado en colaborar o tienes preguntas sobre las funciones de los voluntarios, puedes hablar con el.',
     },
     {
         nombre: 'Luca Emanuel Lopez',
+        tooltip: 'Luca',
         cargo: 'Responsable de Árbitros y Normativas',
         email: 'le_lopez@a.iescalvia.com',
         descripcion: 'Luca se encarga de la asignación de árbitros y del cumplimiento de las normativas del torneo. Cualquier consulta sobre reglas o arbitraje debe dirigirse a él.',
     },
     {
         nombre: 'Juan Jose Gonzalez',
+        tooltip: 'Juanjo',
         cargo: 'Encargado de Material y Equipos',
         email: 'jgonzalez@iescalvia.com',
         descripcion: 'Juanjo es responsable de asegurar que todo el equipo y material necesario esté listo. Desde las redes hasta los balones, el se asegura de que los partidos se jueguen sin problemas.',
