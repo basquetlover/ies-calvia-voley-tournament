2c30d44661d714d615c478f53af729e912c3a0f3
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-11-05 12:44:36 +0100

    Actualizar equipos

--- src/const/equipos-inscritos.ts
+++ src/const/equipos-inscritos.ts
@@ -4,19 +4,43 @@ export const EQUIPOS_INSCRITOS = [
         nombre: 'Team Teto',
         escudo: 'team-teto.png',
         capitan: 'Miguel Socias',
-        entrenador: 'Pere Alemany',
+        entrenador: 'Sergio Ludeña',
         jugadores:[
             {
                 nombre: 'Miguel Socias',
-                curso: '2n Batch'
+                curso: '2º Batch'
             },
             {
                 nombre: 'Lucas Hechavarria',
-                curso: '2n Batch'
+                curso: '2º Batch'
+            },
+            {
+                nombre: 'Gabriel Vidal',
+                curso: '2º Batch'
+            },
+            {
+                nombre: 'Venus Monney',
+                curso: '1º Batch'
             },
             {
                 nombre: 'Luca Emanuel',
-                curso: '2n Batch'
+                curso: '2º Batch'
+            },
+            {
+                nombre: 'Santino Bonavera',
+                curso: '2º Batch'
+            },
+            {
+                nombre: 'Marina Piña',
+                curso: '2º Batch'
+            },
+            {
+                nombre: 'Isa Marin',
+                curso: '1º Batch'
+            },
+            {
+                nombre: 'Patrick George',
+                curso: '2º Batch'
             }
         ]
   
