102cab9ac37cacae3437750432b6538d38b251cc
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-11-09 11:03:14 +0100

    modificar batx a bat i añadir FP

--- src/const/equipos-inscritos.ts
+++ src/const/equipos-inscritos.ts
@@ -10,39 +10,39 @@ export const EQUIPOS_INSCRITOS = [
         jugadores:[
             {
                 nombre: 'Miguel Socias',
-                curso: '2º Batch'
+                curso: '2º Bat'
             },
             {
                 nombre: 'Lucas Hechavarria',
-                curso: '2º Batch'
+                curso: '2º Bat'
             },
             {
                 nombre: 'Gabriel Vidal',
-                curso: '2º Batch'
+                curso: '2º Bat'
             },
             {
                 nombre: 'Venus Monney',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Luca Emanuel',
-                curso: '2º Batch'
+                curso: '2º Bat'
             },
             {
                 nombre: 'Santino Bonavera',
-                curso: '2º Batch'
+                curso: '2º Bat'
             },
             {
                 nombre: 'Marina Piña',
-                curso: '2º Batch'
+                curso: '2º Bat'
             },
             {
                 nombre: 'Isa Marin',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Patrick George',
-                curso: '2º Batch'
+                curso: '2º Bat'
             }
         ]
   
@@ -56,35 +56,35 @@ export const EQUIPOS_INSCRITOS = [
         jugadores:[
             {
                 nombre: 'Leire Ortega',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Jana Gunther',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Laia Dengra',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Ariadna Jaume',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Nico Vargas',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Ana Luna',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Tim Toepfer',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Erika Vera',
-                curso: '1º Batch'
+                curso: '1º Bat'
             },
             {
                 nombre: 'Ainhoa Cumbrerars',
