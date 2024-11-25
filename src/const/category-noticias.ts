141145c19382884788a4c1690bf2f8c669f9f59e
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-11-25 23:42:34 +0100

    crear form
    falta funcionalidad (API)

---
+++ src/const/category-noticias.ts
@@ -0,0 +1,20 @@
+export const CATEGORY = [
+    {
+        value: "Normativa",
+    },
+    {
+        value: "Clasificació",
+    },
+    {
+        value: "Inscripció",
+    },
+    {
+        value: "Equips",
+    },
+    {
+        value: "Bases de Competició",
+    },
+    {
+        value: "Torneig Nadal",
+    }
+]
