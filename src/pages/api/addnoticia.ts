5098c658f21971f16a09474e021d26fff83fa175
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-11-26 20:46:59 +0100

    add noticia

---
+++ src/pages/api/addnoticia.ts
@@ -0,0 +1,101 @@
+import type { APIRoute } from "astro";
+import { supabase, supabaseAdmin } from "../../lib/supabase";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const nombre_noticia = formData.get("nombre_noticia")?.toString().trim() || "";
+  const categoria = formData.get("categoria")?.toString().trim();
+  const fecha = formData.get("fecha")?.toString().trim();
+  const cuerpo = formData.get("cuerpo")?.toString().trim();
+
+  const id_noticia = nombre_noticia.toLowerCase().replace(/\s+/g, '-');
+  
+  // Recoger la información del escudo (imagen)
+  const imagen_noticia = formData.get("imagen_noticia") as File;
+  console.log("Tipo de archivo:", imagen_noticia.name);
+  console.log("Tipo de archivo:", imagen_noticia.type);
+  console.log("Tamaño de archivo:", imagen_noticia.size);
+
+
+  if (!nombre_noticia || !categoria) {
+    return new Response("Correo electrónico y categoria son obligatorios", { status: 400 });
+  }
+
+  console.log("Datos recibidos:", { nombre_noticia, categoria });
+
+  // Verificar si el correo electrónico ya existe en la tabla 'administradores'
+  const { data: existingAdmins, error: checkError } = await supabaseAdmin
+  .from("Noticias")
+  .select("id") // Seleccionar un campo mínimo
+  .eq("titulo", nombre_noticia);
+
+if (checkError) {
+  console.error("Error al verificar la existencia:", checkError.message);
+  return new Response("Hubo un error al verificar el correo electrónico.", { status: 500 });
+}
+
+
+async function uploadFile(file: File, id_noticia: string) {
+    // Extraer la extensión del archivo
+    const extension = file.name.split('.').pop(); // Obtiene la extensión
+    const uniqueFileName = `${id_noticia}.${extension}`; // Combina id_equipo con la extensión
+    const filePath = `${uniqueFileName}`; // Define la ruta del archivo
+  
+    const { data, error } = await supabaseAdmin.storage
+        .from('NoticiasIMG')
+        .upload(filePath, file); // Sube el archivo
+  
+    if (error) {
+        console.error("Error al subir imagen:", error.message);
+        throw new Error("Error al subir imagen.");
+    }
+  
+    console.log("Imagen subida correctamente:", data.path);
+    return filePath; // Devuelve la ruta del archivo
+  }
+  
+  // Llama a la función para subir el imagen_noticia
+  const imagen_noticiaPath = await uploadFile(imagen_noticia, id_noticia);
+  
+  // Obtener la URL pública del imagen_noticia subido
+  const { data: urlData } = supabaseAdmin.storage
+      .from('NoticiasIMG')
+      .getPublicUrl(imagen_noticiaPath); // Usa el imagen_noticiaPath que se generó al subir el archivo
+  
+  // Verifica si urlData contiene la propiedad publicUrl
+  if (!urlData || !urlData.publicUrl) {
+      console.error("No se pudo obtener la URL pública del imagen_noticia.");
+      return new Response("Hubo un error al procesar el imagen_noticia.", { status: 500 });
+  }
+  
+  // Ahora puedes usar urlData.publicUrl para insertar en la base de datos
+  const publicUrl = urlData.publicUrl;
+  console.log("URL pública del imagen_noticia:", publicUrl);
+
+
+if (existingAdmins && existingAdmins.length > 0) {
+  // return new Response("El usuario ya tiene un categoria asignado.", { status: 400 });
+  console.log("El usuario ya tiene un categoria asignado.")
+}
+
+  // Insertar los datos en la tabla 'administradores'
+  const { error: adminError } = await supabaseAdmin
+    .from("Noticias")
+    .insert([
+      {
+        titulo: nombre_noticia,
+        category: categoria,
+        fecha: fecha,
+        contenido: cuerpo,
+        imagen_noticia: publicUrl
+      },
+    ]);
+
+  if (adminError) {
+    console.error("Error insertando en administradores:", adminError.message);
+    return new Response("Hubo un error al asignar el categoria.", { status: 500 });
+  }
+
+  console.log("categoria asignado correctamente");
+  return redirect("/admin/noticias");
+};
