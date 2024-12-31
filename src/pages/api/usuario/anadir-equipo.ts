2ec946336e5bbcd3518b81b345413a7c05739aeb
basquetlover <85557755+basquetlover@users.noreply.github.com>
2024-12-31 16:12:01 +0100

    envio de email

--- src/pages/api/usuario/anadir-equipo.ts
+++ src/pages/api/usuario/anadir-equipo.ts
@@ -1,17 +1,21 @@
 
 //Mi codigo
-
+import { ActionError, defineAction } from 'astro:actions';
 import type { APIRoute } from "astro";
 import { supabase, supabaseAdmin } from "../../../lib/supabase";
+import { Resend } from 'resend';
 
 
-export const POST: APIRoute = async ({ request, redirect }) => {
+export const POST: APIRoute = async ({ request }) => {
   const formData = await request.formData();
   const nombre_equipo = formData.get("nombre_equipo")?.toString().trim() || "";
+  const usuario_nombre = formData.get("usuario_nombre")?.toString().trim() || "";
+  const usuario_email = formData.get("usuario_email")?.toString().trim() || "";
+  const usuario_curso = formData.get("usuario_curso")?.toString().trim() || "";
   const usuario_id = formData.get("usuario_id")?.toString().trim() || "";
   const capitan = formData.get("capitan")?.toString().trim();
 
-
+  let publicUrl = "https://iescalvia-voley.com/img/escudos/sin-escudo.png";
   let acompañante_nombre = "";
   let acompañante_curso = "";
   let acompañante_1r_apellido = "";
@@ -161,7 +165,7 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     index++;
   }
 
-  console.log("Datos usuario:", {usuario_id });
+  console.log("Datos usuario:", {usuario_id, usuario_curso, usuario_email, usuario_nombre });
   console.log("Datos recibidos:", {id_equipo, nombre_equipo, capitan, acompañante, escudo, jugadores,  jugadores_extra });
   if (!capitan) {
     console.log(capitan)
@@ -232,15 +236,19 @@ const { data: urlData } = supabaseAdmin.storage
 
 // Verifica si urlData contiene la propiedad publicUrl
 if (!urlData || !urlData.publicUrl) {
-    console.error("No se pudo obtener la URL pública del escudo.");
-    return new Response(
-      `<div class="bg-red-600 bg-opacity-30 border-3 border-red-700 text-white rounded-lg p-2 my-2 flex items-center text-center">Hi ha hagut un error en processar l'escut. Torna-ho a intentar més tard.</div>`, 
-      { status: 401, headers: { "Content-Type": "text/html" } }
-  );
+  //   console.error("No se pudo obtener la URL pública del escudo.");
+  //   return new Response(
+  //     `<div class="bg-red-600 bg-opacity-30 border-3 border-red-700 text-white rounded-lg p-2 my-2 flex items-center text-center">Hi ha hagut un error en processar l'escut. Torna-ho a intentar més tard.</div>`, 
+  //     { status: 401, headers: { "Content-Type": "text/html" } }
+  // );
+}
+if (urlData) {
+  publicUrl = urlData.publicUrl;
 }
 
+
 // Ahora puedes usar urlData.publicUrl para insertar en la base de datos
-const publicUrl = urlData.publicUrl;
+
 console.log("URL pública del escudo:", publicUrl);
     
     
@@ -345,6 +353,140 @@ console.log("URL pública del escudo:", publicUrl);
 
 
   console.log("Equipo añadido correctamente");
+
+
+  const resend = new Resend(import.meta.env.RESEND_API_KEY);
+  const emailContent = `
+<div style="max-width: 600px; margin: auto; font-family: Arial, sans-serif; background-color: #1a1a1a; color: #ffffff; padding: 20px; border-radius: 8px;">
+    <a href="https://iescalvia-voley.com" style="display: flex; align-items: center; gap: 4px; text-decoration: none;">
+        <img src="https://iescalvia-voley.com/web-app-manifest-192x192.png" alt="Logo IES Calvia voley tournament" style="width: 60px; height:60px;"/>
+        <h1 style="color: #ffc107; font-size:20px; font-weight:bold;">IES Calvià Voley Tournament</h1>
+    </a>
+    <hr style="border: 1px solid #333; border-radius: 10px; margin: 30px 0" />
+    <div style="margin-bottom: 16px; font-family:Roboto Condensed, sans-serif;font-size:32px; font-weight:bold; line-height:38px;text-align:center; color: #1666FF;">
+        Equip inscrit correctament al torneig de Setmana Santa
+    </div>
+    <h2 style="color: #FFC107; font-size: 24px; margin-bottom: 16px;">Equip Inscrit Per:</h2>
+    <div style="display: grid; grid-template-columns: max-content 1fr max-content; align-items: center; place-items: center; text-lg; color: #ffffff; margin-bottom: 20px;">
+      <p style="margin: 0;">${usuario_nombre}</p>
+      <p style="margin: 0;">${usuario_email}</p>
+      <p style="margin: 0;">${usuario_curso}</p>
+    </div>
+    <div style="text-align: center; margin: 20px 0;">
+      <h3 style="font-size: 32px; background-color: transparent; color: #FFC107; padding: 10px; border-radius: 5px;">IES Calvià Voley Team</h3>
+    </div>
+    <div style="width: 240px; height: 240px; margin: auto; border-radius: 10px; overflow: hidden;">
+      <img src=${publicUrl} alt="Logo equipo" style="width: 100%; height: 100%; object-fit: cover;" />
+    </div>
+    <div style="display: flex; align-items: center; margin-top: 16px;">
+      <label style="color: #FFC107; font-size: 20px; margin-right: 8px;">Capità: ${capitan}</label>
+      <p style="background-color: transparent; color: #ffffff; padding: 8px; border-radius: 5px;"></p>
+    </div>
+    
+    <h4 style="margin-top: 20px; font-size: 24px; color: #FFC107;">Entrenador</h4>
+    <div style="margin-bottom: 10px; padding: 10px; background-color: #333; border-radius: 5px;">
+        <div style="display: flex; flex-direction: column; margin-bottom: 10px; padding: 10px; background-color: #333; border-radius: 5px;">
+            <p style="margin: 0; font-weight: bold; color: #ffffff;">${acompañante_nombre} ${acompañante_1r_apellido} ${acompañante_2n_apellido}</p>
+            <p style="color: #ffffff;">Curs: ${acompañante_curso} </p>
+            <p style="color: #ffffff; text-decoration:none;">Email: ${acompañante_email}</p>
+        </div>
+    </div>
+    
+    <h4 style="margin-top: 20px; font-size: 24px; color: #FFC107;">Alumnes Jugadors</h4>
+    ${jugadores.map(jugador => `
+      <div style="margin-bottom: 10px; padding: 10px; background-color: #333; border-radius: 5px;">
+        <div style="display: flex; flex-direction: column; margin-bottom: 10px; padding: 10px; background-color: #333; border-radius: 5px;">
+            <p style="margin: 0; font-weight: bold; color: #ffffff;">${jugador.nombre} ${jugador._1r_apellido} ${jugador._2n_apellido}</p>
+            <p style="color: #ffffff;">Curs: ${jugador.curso}</p>
+            <p style="color: #ffffff;">Email: ${jugador.email}</p>
+        </div>
+      </div>
+    `).join('')}
+    ${jugadores_extra.map(jugador => `
+      <div style="margin-bottom: 10px; padding: 10px; background-color: #333; border-radius: 5px;">
+        <div style="display: flex; flex-direction: column; margin-bottom: 10px; padding: 10px; background-color: #333; border-radius: 5px;">
+            <p style="margin: 0; font-weight: bold; color: #ffffff;">${jugador.nombre} ${jugador._1r_apellido} ${jugador._2n_apellido}</p>
+            <p style="color: #ffffff;">Curs: ${jugador.curso}</p>
+            <p style="color: #ffffff;">Email: ${jugador.email}</p>
+        </div>
+      </div>
+    `).join('')}
+
+    <hr style="border: 1px solid #ffc107; border-radius: 10px; margin: 30px 0" />
+    <div style="width: 100%;">
+      <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
+          <h4 style="color: #1666FF; font-size: 22px; white-space: nowrap; font-weight: 600; text-transform: uppercase;">Organitzat per</h4>
+          <div style="display:grid; grid-template-columns: 1fr 1fr; grid-template-rows: 1fr; gap: 4px;">
+              <a>
+                  <img src="https://iescalvia-voley.com/img/team-teto.png" style="width: 175px; height: 175px;" alt="TEAM Teto" />
+              </a>
+              <a href="https://sites.google.com/iescalvia.com/iescalvia/inici" target="_blank">
+                  <img src="https://iescalvia-voley.com/img/ies-calvia.png" style="width: 175px; height: 175px;" alt="IES Calvi=C3=A0" />
+              </a>
+          </div>
+      </div>
+      
+       <div style="display: flex; flex-direction: row; justify-items: center; align-items: center; font-size: 16px;">
+          <div style="display: flex; flex-direction: row; justify-content: center; align-items: center;">
+            <img src="https://iescalvia-voley.com/img/licencia/cc.png" style="width: 20px; height:20px; margin: 0 4px;" alt="Creative Comons"/>
+            2024 - <h2 style="margin-left: 4px; color:#ffffff; font-size:16px;">IES Calvià Voley Tournament</h2>
+          </div>
+          <span style="margin: 0 4px;">|</span>
+          <span>
+          <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/?refselecter-v1" style="display: flex; flex-direction: row; justify-content: center; align-items: center; font-size: 16px; color: #fff;" target="_blank" rel="licencia noopener noreferrer">Tots els drets reservats.
+            <img src="https://iescalvia-voley.com/img/licencia/attribution.png" style="width: 20px; height:20px; margin: 0 4px;" alt="Attribution"/>
+            <img src="https://iescalvia-voley.com/img/licencia/nc.png" style="width: 20px; height:20px; margin: 0 4px;" alt="NonCommercial"/>
+            <img src="https://iescalvia-voley.com/img/licencia/nd.png" style="width: 20px; height:20px; margin: 0 4px;" alt="NoDerivatives"/>
+          </a>
+        </span>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+try {
+  const { data, error } = await resend.emails.send({
+    from: 'IES Calvià Voley Tournament <onboarding@resend.dev>',
+    to: [usuario_email], // Asegúrate de que esta variable tenga el valor correcto
+    subject: `Inscripció Realitzada de ${nombre_equipo}`,
+    html: emailContent,
+  });
+
+  if (error) {
+    throw new Error(error.message); // Lanza un error si hay un problema
+  }
+
+  console.log("Correo enviado correctamente", data);
+} catch (error) {
+  console.error("Error al enviar el correo:", error);
+}
+
+// try {
+//   send: defineAction({
+//     accept: 'form',
+//     handler: async () => {
+//       const { data, error } = await resend.emails.send({
+//         from: 'IES Calvià Voley Tournament <onboarding@resend.dev>',
+//         to: [usuario_email],
+//         subject: `Inscripció Realitzada de ${nombre_equipo}`,
+//         html: emailContent,
+//       });
+
+//       if (error) {
+//         throw new ActionError({
+//           code: 'BAD_REQUEST',
+//           message: error.message,
+//         });
+//       }
+
+//       return data;
+//     },
+//   }),
+//   console.log("Correo enviado correctamente");
+// } catch (error) {
+//   console.error("Error al enviar el correo:");
+// }
+
   return new Response(
     JSON.stringify({ success: true }), 
     { status: 200, headers: { "Content-Type": "application/json" } }
