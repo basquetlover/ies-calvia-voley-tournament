4697e549b184ac1d6648da5a1abe9142b9cb6554
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-01-02 19:54:56 +0100

    add galeria

---
+++ src/const/imagenes-torneo-navidad.ts
@@ -0,0 +1,662 @@
+export const IMAGENES = [
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6974.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6949.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6947.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6939.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6935.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6932.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6928.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6917.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6912.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6907.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6902.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6905.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6896.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6869.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6866.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6861.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6862.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6830.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6828.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6807.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6726.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6725.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6696.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6689.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6676.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6661.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6670.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6645.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6635.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6641.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6626.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6614.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6602.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6566.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6569.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6560.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6555.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6536.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6535.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6517.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6516.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6488.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6505.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6487.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6470.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6465.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6460.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6450.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6453.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6435.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6418.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6424.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6416.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6412.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6411.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6410.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6375.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6382.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6370.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6359.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6350.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6336.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6340.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6293.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6307.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6291.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6287.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6286.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6252.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6217.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6186.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6182.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6156.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6179.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6154.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6155.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6135.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6104.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6106.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6079.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6032.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR6025.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5993.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5975.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5971.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5943.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5939.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5934.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5925.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5917.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5910.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5893.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5868.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5844.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5843.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5839.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5841.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5830.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5835.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5820.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5815.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5819.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5805.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5776.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5804.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5753.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5751.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5746.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5739.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5731.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5729.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5699.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5690.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5689.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5687.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5679.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5676.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5674.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5661.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5650.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5655.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5640.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5604.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5590.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5560.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5554.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5533.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5546.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5530.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5523.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5529.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5487.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5486.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5475.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5461.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5467.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5455.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5453.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5452.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5434.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5398.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5393.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5383.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5370.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5372.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5363.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5348.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5334.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5330.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5326.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5323.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5302.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5298.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5297.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5296.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5285.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5259.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5265.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5258.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5244.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5241.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5229.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5228.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5210.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5226.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5191.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5189.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5184.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5182.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5172.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5161.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5148.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5160.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5138.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5107.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5112.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5100.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5097.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5092.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5082.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5081.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5072.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5068.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5063.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5057.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5053.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5055.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5052.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5051.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5038.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5031.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5025.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5024.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4999.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5018.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4960.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4939.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4916.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4909.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4877.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4879.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4855.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4862.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4850.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4844.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4837.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4835.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4833.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4830.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4817.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4816.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4823.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4790.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4793.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4775.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4735.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4764.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4705.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4718.jpg',
+},
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR4729.jpg',
+}
+]
