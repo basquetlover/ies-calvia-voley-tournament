35227b2dd42fb0a1d7e6d7c2b0ad7cb5125b04f1
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-01-03 21:00:09 +0100

    Update imagenes-torneo-navidad.ts

--- src/const/imagenes-torneo-navidad.ts
+++ src/const/imagenes-torneo-navidad.ts
@@ -284,9 +284,9 @@ export const IMAGENES = [
 {
  enlace: '/img/torneo-navidad/20122024-_MSR5843.jpg',
 },
-// {
-//  enlace: '/img/torneo-navidad/20122024-_MSR5839.jpg',
-// },
+{
+ enlace: '/img/torneo-navidad/20122024-_MSR5839.jpg',
+},
 {
  enlace: '/img/torneo-navidad/20122024-_MSR5841.jpg',
 },
@@ -350,9 +350,9 @@ export const IMAGENES = [
 {
  enlace: '/img/torneo-navidad/20122024-_MSR5676.jpg',
 },
-{
- enlace: '/img/torneo-navidad/20122024-_MSR5674.jpg',
-},
+// {
+//  enlace: '/img/torneo-navidad/20122024-_MSR5674.jpg',
+// },
 {
  enlace: '/img/torneo-navidad/20122024-_MSR5661.jpg',
 },
