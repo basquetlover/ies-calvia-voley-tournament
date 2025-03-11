b16332e7572d3f3927888ff6ffd0dfca8f4abcc4
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-03-11 12:18:06 +0100

    arreglar evaluar

--- src/pages/api/usuario/evaluar.ts
+++ src/pages/api/usuario/evaluar.ts
@@ -31,7 +31,21 @@ export const POST: APIRoute = async ({ request }) => {
         const entrenador_id = formData.get("entrenador_id")?.toString().trim();
         const img_entrenador = formData.get("img-entrenador")?.toString().trim();
         const probl_entrenador = formData.get("probl-entrenador")?.toString().trim();
-
+        if(img_entrenador === ''){
+          return new Response(
+              `
+              <div class="w-[400px] min-h-20 h-max rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 py-1 px-3 border-solid border-2 border-[#A83434] bg-[#A83434] bg-opacity-60 text-base font-semibold">
+                <span>
+              <svg xmlns="http://www.w3.org/2000/svg"  class="fill-[#BA3A3A] w-16 h-16" viewBox="0 -960 960 960">
+                <path d="m332-285 148-148 148 148 47-47-148-148 148-148-47-47-148 148-148-148-47 47 148 148-148 148 47 47ZM480-80q-82 0-155-31-73-32-128-86-54-55-85-128T80-480q0-83 32-156t85-127q55-54 128-85t155-32q83 0 156 32t127 85q54 54 86 127t31 156q0 82-31 155-32 73-86 128-54 54-127 86T480-80Z"/>
+              </svg>
+                </span>
+                <p>Es necesario indicar si la foto es valida del entrenador</p>
+                </div>
+              `, 
+              { status: 401, headers: { "Content-Type": "text/html" } }
+          );
+       }
         
 
         const jugadores = [];
