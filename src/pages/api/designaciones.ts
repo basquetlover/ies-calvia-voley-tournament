3abd77fd1ece7b10717e908e64ff75e893277944
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-04-08 00:03:12 +0200

    crear designaciones

--- src/pages/api/designaciones.ts
+++ src/pages/api/designaciones.ts
@@ -7,8 +7,8 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     let index = 0; // Comienza en 0 para que coincida con el índice del frontend
     while (formData.has(`p1_id_${index}`)) {
         const arbitro = formData.get(`p1_arbitro_${index}`);
-        const oficial_mesa = formData.get(`p1_oficial_mesa_${index}`);
-        const oficial_mesa_2 = formData.get(`p1_oficial_mesa_2_${index}`)?.toString().trim();
+        const oficial_mesa = formData.get(`p1_oficial_${index}`);
+        const oficial_mesa_2 = formData.get(`p1_oficial_2_${index}`)?.toString().trim();
         const id_partido = formData.get(`p1_id_${index}`); // Asegúrate de que el arbitro coincida
 
         
@@ -21,8 +21,8 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     index = 0; // Comienza en 0 para que coincida con el índice del frontend
     while (formData.has(`p2_id_${index}`)) {
         const arbitro = formData.get(`p2_arbitro_${index}`);
-        const oficial_mesa = formData.get(`p2_oficial_mesa_${index}`);
-        const oficial_mesa_2 = formData.get(`p2_oficial_mesa_2_${index}`)?.toString().trim();
+        const oficial_mesa = formData.get(`p2_oficial_${index}`);
+        const oficial_mesa_2 = formData.get(`p2_oficial_2_${index}`)?.toString().trim();
         const id_partido = formData.get(`p2_id_${index}`); // Asegúrate de que el arbitro coincida
 
         
@@ -31,16 +31,30 @@ export const POST: APIRoute = async ({ request, redirect }) => {
         index++;
     }
 
-    console.log(designaciones2)
+    const designaciones3 = [];
+    index = 0; // Comienza en 0 para que coincida con el índice del frontend
+    while (formData.has(`p_central_id_${index}`)) {
+        const arbitro = formData.get(`p_central_arbitro_${index}`);
+        const oficial_mesa = formData.get(`p_central_oficial_${index}`);
+        const oficial_mesa_2 = formData.get(`p_central_oficial_2_${index}`)?.toString().trim();
+        const id_partido = formData.get(`p_central_id_${index}`); // Asegúrate de que el arbitro coincida
+
+        
+            designaciones3.push({ arbitro, oficial_mesa_2, id_partido, oficial_mesa });
+        
+        index++;
+    }
+
+    //console.log(designaciones2)
 
     for (const partido of designaciones1) {
         const { error: partidoError } = await supabaseAdmin
-          .from('ResultadoNavidad')
+          .from('PartidosSS')
           .update([
             { 
                 arbitro: partido.arbitro, 
-                oficial_mesa_1: partido.oficial_mesa,
-                oficial_mesa_2: partido.oficial_mesa_2,
+                oficial_1: partido.oficial_mesa,
+                oficial_2: partido.oficial_mesa_2,
               },
           ])
           .eq("id", partido.id_partido )
@@ -54,12 +68,12 @@ export const POST: APIRoute = async ({ request, redirect }) => {
 
     for (const partido of designaciones2) {
         const { error: partidoError } = await supabaseAdmin
-          .from('ResultadoNavidad')
+          .from('PartidosSS')
           .update([
             { 
                 arbitro: partido.arbitro, 
-                oficial_mesa_1: partido.oficial_mesa,
-                oficial_mesa_2: partido.oficial_mesa_2,
+                oficial_1: partido.oficial_mesa,
+                oficial_2: partido.oficial_mesa_2,
               },
           ])
           .eq("id", partido.id_partido )
@@ -67,6 +81,25 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     
         if (partidoError) {
           console.error("Error insertando partido principal:", partidoError.message);
+          // Considera si quieres detener todo el proceso o continuar con los siguientes jugadores
+        }
+      }
+
+      for (const partido of designaciones3) {
+        const { error: partidoError } = await supabaseAdmin
+          .from('PartidosSS')
+          .update([
+            { 
+                arbitro: partido.arbitro, 
+                oficial_1: partido.oficial_mesa,
+                oficial_2: partido.oficial_mesa_2,
+              },
+          ])
+          .eq("id", partido.id_partido )
+          .select()
+    
+        if (partidoError) {
+          console.log("Error insertando partido principal:", partidoError.message);
           // Considera si quieres detener todo el proceso o continuar con los siguientes jugadores
         }
       }
