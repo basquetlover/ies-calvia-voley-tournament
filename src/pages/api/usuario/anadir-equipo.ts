f4c709849e0831b20b330cc933cc95daafeafa52
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-06-05 11:16:34 +0200

    Update anadir-equipo.ts

--- src/pages/api/usuario/anadir-equipo.ts
+++ src/pages/api/usuario/anadir-equipo.ts
@@ -81,7 +81,7 @@ export const POST: APIRoute = async ({ request }) => {
   //  console.log("Entrenador no inscrito en otro equipo")
 //}
 let { data: Usuarios, error } = await supabaseAdmin
-    .from(`JugadoresV`)
+    .from("JugadoresV")
     .select('email')
 
     if (Usuarios) {
@@ -156,7 +156,7 @@ if (profesor_email) {
 //  console.log("Entrenador no inscrito en otro equipo")
 //}
 let { data: Usuarios, error } = await supabaseAdmin
-  .from(`JugadoresV`)
+  .from("JugadoresV")
   .select('email')
 
   if (Usuarios) {
@@ -247,7 +247,7 @@ if (!acompañante_genero) {
       mujeres++;
     }
     let { data: Usuarios, error } = await supabaseAdmin
-    .from(`JugadoresV`)
+    .from("JugadoresV")
     .select('email')
 
     if (Usuarios) {
@@ -309,7 +309,7 @@ if (!acompañante_genero) {
         mujeres++;
       }
       let { data: Usuarios, error } = await supabaseAdmin
-    .from(`JugadoresV`)
+    .from("JugadoresV")
     .select('email')
 
     if (Usuarios) {
@@ -366,7 +366,7 @@ if (!acompañante_genero) {
       
       }
       let { data: Usuarios, error } = await supabaseAdmin
-    .from(`JugadoresV`)
+    .from("JugadoresV")
     .select('email')
 
     if (Usuarios) {
@@ -406,9 +406,9 @@ if (!acompañante_genero) {
   }
   
 
-  // Verificar si el equipo ya existe en la tabla `EquiposV`
+  // Verificar si el equipo ya existe en la tabla "EquiposV"
   const { data: existingEquipo, error: checkError } = await supabaseAdmin
-  .from("`EquiposV`")
+  .from(""EquiposV"")
   .select("id") // Seleccionar un campo mínimo
   .eq("nombre_equipo", nombre_equipo);
 
@@ -528,7 +528,7 @@ console.log(currentDate);
     
   // Insertar los datos en la tabla 'administradores'
    const { data: datosEquipos, error: equipoError } = await supabaseAdmin
-    .from(`EquiposV`)
+    .from("EquiposV")
     .insert([
         { nombre_equipo: nombre_equipo ,
           id_equipo: id_equipo ,
@@ -553,7 +553,7 @@ console.log(currentDate);
   }
 
   const { data: equipoData, error: busquedaError } = await supabaseAdmin
-    .from(`EquiposV`)
+    .from("EquiposV")
     .select('id')
     .eq('nombre_equipo', nombre_equipo)
     .single();
@@ -591,7 +591,7 @@ console.log(currentDate);
   for (const jugador of jugadores) {
     //if(jugador.img.size <= 0){
       const { error: jugadorError } = await supabaseAdmin
-        .from(`JugadoresV`)
+        .from("JugadoresV")
         .insert([
           {   nombre: jugador.nombre, 
               _1r_apellido: jugador._1r_apellido,
@@ -632,7 +632,7 @@ console.log(currentDate);
     // }
   
     //   const { error: jugadorError } = await supabaseAdmin
-    //     .from(`JugadoresV`)
+    //     .from("JugadoresV")
     //     .insert([
     //       {   nombre: jugador.nombre, 
     //           _1r_apellido: jugador._1r_apellido,
@@ -656,7 +656,7 @@ console.log(currentDate);
   for (const jugador of jugadores_extra) {
     //if(jugador.img.size <= 0){
       const { error: jugadorError } = await supabaseAdmin
-        .from(`JugadoresV`)
+        .from("JugadoresV")
         .insert([
           {   nombre: jugador.nombre, 
               _1r_apellido: jugador._1r_apellido,
@@ -697,7 +697,7 @@ console.log(currentDate);
     // }
   
     //   const { error: jugadorError } = await supabaseAdmin
-    //     .from(`JugadoresV`)
+    //     .from("JugadoresV")
     //     .insert([
     //       {   nombre: jugador.nombre, 
     //           _1r_apellido: jugador._1r_apellido,
@@ -721,7 +721,7 @@ console.log(currentDate);
   for (const jugador of staff) {
    // if(jugador.img.size <= 0){
     const { error: jugadorError } = await supabaseAdmin
-      .from(`JugadoresV`)
+      .from("JugadoresV")
       .insert([
         {   nombre: jugador.nombre, 
             _1r_apellido: jugador._1r_apellido,
@@ -762,7 +762,7 @@ console.log(currentDate);
   // }
 
   //   const { error: jugadorError } = await supabaseAdmin
-  //     .from(`JugadoresV`)
+  //     .from("JugadoresV")
   //     .insert([
   //       {   nombre: jugador.nombre, 
   //           _1r_apellido: jugador._1r_apellido,
@@ -786,7 +786,7 @@ console.log(currentDate);
 
   //if(acompañante_foto.size <= 0){
     const { error: jugadorExtraError } = await supabaseAdmin
-      .from(`JugadoresV`)
+      .from("JugadoresV")
       .insert([
         { nombre: acompañante_nombre, 
           _1r_apellido: acompañante_1r_apellido,
@@ -805,7 +805,7 @@ console.log(currentDate);
     }
   // } else {
   //   const { error: jugadorExtraError } = await supabaseAdmin
-  //     .from(`JugadoresV`)
+  //     .from("JugadoresV")
   //     .insert([
   //       { nombre: acompañante_nombre, 
   //         _1r_apellido: acompañante_1r_apellido,
@@ -867,7 +867,7 @@ console.log(currentDate);
     //   }
 
   //     const { error: jugadorError } = await supabaseAdmin
-  //     .from(`JugadoresV`)
+  //     .from("JugadoresV")
   //     .insert([
   //       {   nombre: profesor_nombre, 
   //         _1r_apellido: profesor_1r_apellido,
@@ -884,7 +884,7 @@ console.log(currentDate);
 
   // }else{
     const { error: jugadorError } = await supabaseAdmin
-    .from(`JugadoresV`)
+    .from("JugadoresV")
     .insert([
       {   nombre: profesor_nombre, 
         _1r_apellido: profesor_1r_apellido,
