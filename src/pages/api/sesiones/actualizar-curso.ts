4c41c01235b136d4b65147a976b97859c0449ddf
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-09-09 17:02:11 +0200

    actualizar datos usuario cada curso

--- src/pages/api/sesiones/actualizar-curso.ts
+++ src/pages/api/sesiones/actualizar-curso.ts
@@ -14,10 +14,14 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     let anyo_actual = formData.get("anyo_actual")?.toString();
     const id_usuario = formData.get("id_usuario")?.toString();
     const nuevo_curso = formData.get("nuevo_curso")?.toString();
+    const nombre_real = formData.get("nombre_real")?.toString();
+    const email = formData.get("email")?.toString();
+    const apellidos = formData.get("apellidos")?.toString();
+
 
 
     // Validar que se proporcionen nombre y contraseña
-    if (!id_usuario) {
+    if (!email) {
         return new Response(
           `
           <div class="w-full h-full rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 mx-auto py-1 px-3 border-solid border-2 border-[#640404] bg-[#A83434] bg-opacity-100 text-sm font-semibold">
@@ -43,6 +47,36 @@ export const POST: APIRoute = async ({ request, redirect }) => {
       </svg>
         </span>
         <p class="text-[#640404]">No s'ha indicat el nou curs</p>
+        </div>
+          `, 
+          { status: 400, headers: { "Content-Type": "text/html" } }
+        );
+    }
+    if (!nombre_real || nombre_real === "") {
+        return new Response(
+          `
+          <div class="w-full h-full rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 mx-auto py-1 px-3 border-solid border-2 border-[#640404] bg-[#A83434] bg-opacity-100 text-sm font-semibold">
+        <span>
+      <svg xmlns="http://www.w3.org/2000/svg"  class="fill-[#640404] w-10 h-10" viewBox="0 -960 960 960">
+        <path d="m332-285 148-148 148 148 47-47-148-148 148-148-47-47-148 148-148-148-47 47 148 148-148 148 47 47ZM480-80q-82 0-155-31-73-32-128-86-54-55-85-128T80-480q0-83 32-156t85-127q55-54 128-85t155-32q83 0 156 32t127 85q54 54 86 127t31 156q0 82-31 155-32 73-86 128-54 54-127 86T480-80Z"/>
+      </svg>
+        </span>
+        <p class="text-[#640404]">No s'ha indicat el nom de l'usuari</p>
+        </div>
+          `, 
+          { status: 400, headers: { "Content-Type": "text/html" } }
+        );
+    }
+    if (!apellidos || apellidos === "") {
+        return new Response(
+          `
+          <div class="w-full h-full rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 mx-auto py-1 px-3 border-solid border-2 border-[#640404] bg-[#A83434] bg-opacity-100 text-sm font-semibold">
+        <span>
+      <svg xmlns="http://www.w3.org/2000/svg"  class="fill-[#640404] w-10 h-10" viewBox="0 -960 960 960">
+        <path d="m332-285 148-148 148 148 47-47-148-148 148-148-47-47-148 148-148-148-47 47 148 148-148 148 47 47ZM480-80q-82 0-155-31-73-32-128-86-54-55-85-128T80-480q0-83 32-156t85-127q55-54 128-85t155-32q83 0 156 32t127 85q54 54 86 127t31 156q0 82-31 155-32 73-86 128-54 54-127 86T480-80Z"/>
+      </svg>
+        </span>
+        <p class="text-[#640404]">No s'ha indicat els llinatges de l'usuari</p>
         </div>
           `, 
           { status: 400, headers: { "Content-Type": "text/html" } }
@@ -78,8 +112,8 @@ export const POST: APIRoute = async ({ request, redirect }) => {
      
     const { data } = await supabaseAdmin
     .from('Usuarios')
-    .update({ curso:  nuevo_curso, anyo: anyo_actual})
-    .eq('id', id_usuario)
+    .update({ curso:  nuevo_curso, anyo: anyo_actual, nombre_real: nombre_real, apellidos: apellidos})
+    .eq('email', email)
     .select()
 
     // Redirigir a la página principal
