d6602c87f39d08e5e4ab133612bff317119d7c5d
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-11-09 14:31:11 +0100

    arreglar definitivamente

--- src/pages/api/inscripcion/jugadores.ts
+++ src/pages/api/inscripcion/jugadores.ts
@@ -121,89 +121,89 @@ export const POST: APIRoute = async ({ request }) => {
       }
 
       //3.- Recoger la información de los jugadores extra
-          const jugadores_extra = [];
-          index = 0;
-          while (formData.has(`extra_player_${index}_name`)) {
-            const nombre = formData.get(`extra_player_${index}_name`)?.toString().trim();
-            if(nombre !== ""){
-              const _1r_apellido = formData.get(`extra_player_${index }_1r_apellido`)?.toString().trim();
-              const _2n_apellido = formData.get(`extra_player_${index}_2n_apellido`)?.toString().trim();
-              const genero_extra = formData.get(`extra_genero_${index}`)?.toString().trim();
-              const curso = formData.get(`extra_player_${index}_curso`)?.toString().trim();
-              const email = formData.get(`extra_player_${index}_email`)?.toString().trim();
-              const img = formData.get(`extra_player_${index}_img`) as File;
-              const numero = index + 7;
-              if (email) { 
-                const dominio = email.split('@')[1]; // Esto te dará 'gmail.com'
-                const dominioConArroba = '@' + dominio;
-                if (dominioConArroba !== "@alu.ibeducacio.eu" && dominioConArroba !== "@ibeducacio.eu") {
-                  console.log(`El email del ${index + 7}. Jugador ha de ser del centre`)
-                  return new Response(
-                        `<div class="w-full h-full rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 mx-auto py-1 px-3 border-solid border-2 border-[#640404] bg-[#A83434] bg-opacity-100 text-sm font-semibold">
-                <span>
-              <svg xmlns="http://www.w3.org/2000/svg"  class="fill-[#640404] w-10 h-10" viewBox="0 -960 960 960">
-                <path d="m332-285 148-148 148 148 47-47-148-148 148-148-47-47-148 148-148-148-47 47 148 148-148 148 47 47ZM480-80q-82 0-155-31-73-32-128-86-54-55-85-128T80-480q0-83 32-156t85-127q55-54 128-85t155-32q83 0 156 32t127 85q54 54 86 127t31 156q0 82-31 155-32 73-86 128-54 54-127 86T480-80Z"/>
-              </svg>
-                </span>
-                <p class="text-[#640404]">El email del ${index + 7}. Jugador ha de ser del centre</p>
-                </div>`, 
-                        { status: 401, headers: { "Content-Type": "text/html" } }
-                    );
-              }
-            } else {
-                // Manejo del caso en que email es undefined o vacío
-                console.error("El email no es válido.");
-            }
-              if(!genero_extra) {
-                return new Response(
-                  `<div class="w-full h-full rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 mx-auto py-1 px-3 border-solid border-2 border-[#640404] bg-[#A83434] bg-opacity-100 text-sm font-semibold">
-                <span>
-              <svg xmlns="http://www.w3.org/2000/svg"  class="fill-[#640404] w-10 h-10" viewBox="0 -960 960 960">
-                <path d="m332-285 148-148 148 148 47-47-148-148 148-148-47-47-148 148-148-148-47 47 148 148-148 148 47 47ZM480-80q-82 0-155-31-73-32-128-86-54-55-85-128T80-480q0-83 32-156t85-127q55-54 128-85t155-32q83 0 156 32t127 85q54 54 86 127t31 156q0 82-31 155-32 73-86 128-54 54-127 86T480-80Z"/>
-              </svg>
-                </span>
-                <p class="text-[#640404]">Seleccioni el gènere del ${index + 7}. Jugador ${nombre}</p>
-                </div>`, 
-                  { status: 401, headers: { "Content-Type": "text/html" } }
-              );
+          // const jugadores_extra = [];
+          // index = 0;
+          // while (formData.has(`extra_player_${index}_name`)) {
+          //   const nombre = formData.get(`extra_player_${index}_name`)?.toString().trim();
+          //   if(nombre !== ""){
+          //     const _1r_apellido = formData.get(`extra_player_${index }_1r_apellido`)?.toString().trim();
+          //     const _2n_apellido = formData.get(`extra_player_${index}_2n_apellido`)?.toString().trim();
+          //     const genero_extra = formData.get(`extra_genero_${index}`)?.toString().trim();
+          //     const curso = formData.get(`extra_player_${index}_curso`)?.toString().trim();
+          //     const email = formData.get(`extra_player_${index}_email`)?.toString().trim();
+          //     const img = formData.get(`extra_player_${index}_img`) as File;
+          //     const numero = index + 7;
+          //     if (email) { 
+          //       const dominio = email.split('@')[1]; // Esto te dará 'gmail.com'
+          //       const dominioConArroba = '@' + dominio;
+          //       if (dominioConArroba !== "@alu.ibeducacio.eu" && dominioConArroba !== "@ibeducacio.eu") {
+          //         console.log(`El email del ${index + 7}. Jugador ha de ser del centre`)
+          //         return new Response(
+          //               `<div class="w-full h-full rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 mx-auto py-1 px-3 border-solid border-2 border-[#640404] bg-[#A83434] bg-opacity-100 text-sm font-semibold">
+          //       <span>
+          //     <svg xmlns="http://www.w3.org/2000/svg"  class="fill-[#640404] w-10 h-10" viewBox="0 -960 960 960">
+          //       <path d="m332-285 148-148 148 148 47-47-148-148 148-148-47-47-148 148-148-148-47 47 148 148-148 148 47 47ZM480-80q-82 0-155-31-73-32-128-86-54-55-85-128T80-480q0-83 32-156t85-127q55-54 128-85t155-32q83 0 156 32t127 85q54 54 86 127t31 156q0 82-31 155-32 73-86 128-54 54-127 86T480-80Z"/>
+          //     </svg>
+          //       </span>
+          //       <p class="text-[#640404]">El email del ${index + 7}. Jugador ha de ser del centre</p>
+          //       </div>`, 
+          //               { status: 401, headers: { "Content-Type": "text/html" } }
+          //           );
+          //     }
+          //   } else {
+          //       // Manejo del caso en que email es undefined o vacío
+          //       console.error("El email no es válido.");
+          //   }
+          //     if(!genero_extra) {
+          //       return new Response(
+          //         `<div class="w-full h-full rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 mx-auto py-1 px-3 border-solid border-2 border-[#640404] bg-[#A83434] bg-opacity-100 text-sm font-semibold">
+          //       <span>
+          //     <svg xmlns="http://www.w3.org/2000/svg"  class="fill-[#640404] w-10 h-10" viewBox="0 -960 960 960">
+          //       <path d="m332-285 148-148 148 148 47-47-148-148 148-148-47-47-148 148-148-148-47 47 148 148-148 148 47 47ZM480-80q-82 0-155-31-73-32-128-86-54-55-85-128T80-480q0-83 32-156t85-127q55-54 128-85t155-32q83 0 156 32t127 85q54 54 86 127t31 156q0 82-31 155-32 73-86 128-54 54-127 86T480-80Z"/>
+          //     </svg>
+          //       </span>
+          //       <p class="text-[#640404]">Seleccioni el gènere del ${index + 7}. Jugador ${nombre}</p>
+          //       </div>`, 
+          //         { status: 401, headers: { "Content-Type": "text/html" } }
+          //     );
               
-              }
-              if(genero_extra === "hombre"){
-                hombres++;
-              }
+          //     }
+          //     if(genero_extra === "hombre"){
+          //       hombres++;
+          //     }
               
-              if(genero_extra === "mujer"){
-                mujeres++;
-              }
-              let { data: Usuarios, error } = await supabaseAdmin
-            .from(TablaJugadores)
-            .select('email')
+          //     if(genero_extra === "mujer"){
+          //       mujeres++;
+          //     }
+          //     let { data: Usuarios, error } = await supabaseAdmin
+          //   .from(TablaJugadores)
+          //   .select('email')
 
-            if (Usuarios) {
+          //   if (Usuarios) {
           
             
-              // Verificar si ya existe un usuario con el mismo email
-              const userExistsByEmail = Usuarios.some(usuario => usuario.email === email);
+          //     // Verificar si ya existe un usuario con el mismo email
+          //     const userExistsByEmail = Usuarios.some(usuario => usuario.email === email);
               
-              if (userExistsByEmail) {
-                return new Response(
-                  `<div class="w-full h-full rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 mx-auto py-1 px-3 border-solid border-2 border-[#640404] bg-[#A83434] bg-opacity-100 text-sm font-semibold">
-                <span>
-              <svg xmlns="http://www.w3.org/2000/svg"  class="fill-[#640404] w-10 h-10" viewBox="0 -960 960 960">
-                <path d="m332-285 148-148 148 148 47-47-148-148 148-148-47-47-148 148-148-148-47 47 148 148-148 148 47 47ZM480-80q-82 0-155-31-73-32-128-86-54-55-85-128T80-480q0-83 32-156t85-127q55-54 128-85t155-32q83 0 156 32t127 85q54 54 86 127t31 156q0 82-31 155-32 73-86 128-54 54-127 86T480-80Z"/>
-              </svg>
-                </span>
-                <p class="text-[#640404]">El jugador Nº${index +7} ja es troba inscrit</p>
-                </div>`, 
-                  { status: 400, headers: { "Content-Type": "text/html" } }
-                );
-              }
-            }
-              jugadores_extra.push({ nombre, curso, _1r_apellido, _2n_apellido, genero_extra, email, numero, img });
-            }
-            index++;
-          }
-          console.log("Jugadores Extra:", jugadores_extra);
+          //     if (userExistsByEmail) {
+          //       return new Response(
+          //         `<div class="w-full h-full rounded-lg grid grid-rows-1 grid-cols-[max-content_1fr] items-center gap-2 mx-auto py-1 px-3 border-solid border-2 border-[#640404] bg-[#A83434] bg-opacity-100 text-sm font-semibold">
+          //       <span>
+          //     <svg xmlns="http://www.w3.org/2000/svg"  class="fill-[#640404] w-10 h-10" viewBox="0 -960 960 960">
+          //       <path d="m332-285 148-148 148 148 47-47-148-148 148-148-47-47-148 148-148-148-47 47 148 148-148 148 47 47ZM480-80q-82 0-155-31-73-32-128-86-54-55-85-128T80-480q0-83 32-156t85-127q55-54 128-85t155-32q83 0 156 32t127 85q54 54 86 127t31 156q0 82-31 155-32 73-86 128-54 54-127 86T480-80Z"/>
+          //     </svg>
+          //       </span>
+          //       <p class="text-[#640404]">El jugador Nº${index +7} ja es troba inscrit</p>
+          //       </div>`, 
+          //         { status: 400, headers: { "Content-Type": "text/html" } }
+          //       );
+          //     }
+          //   }
+          //     jugadores_extra.push({ nombre, curso, _1r_apellido, _2n_apellido, genero_extra, email, numero, img });
+          //   }
+          //   index++;
+          // }
+          // console.log("Jugadores Extra:", jugadores_extra);
           console.log("Jugadores:", jugadores);
 
 
@@ -323,63 +323,63 @@ export const POST: APIRoute = async ({ request }) => {
     }
   }
 
-  for (const jugador of jugadores_extra) {
-    if(jugador.img.size <= 0){
-      const { error: jugadorError } = await supabaseAdmin
-        .from(TablaJugadores)
-        .insert([
-          {   nombre: jugador.nombre, 
-              _1r_apellido: jugador._1r_apellido,
-              _2n_apellido: jugador._2n_apellido,
-              curso: jugador.curso,
-              genero: jugador.genero_extra,
-              pertenece_equipo: Equipo_id,
-              email: jugador.email,
-              ficha: 'jugador',
+  // for (const jugador of jugadores_extra) {
+  //   if(jugador.img.size <= 0){
+  //     const { error: jugadorError } = await supabaseAdmin
+  //       .from(TablaJugadores)
+  //       .insert([
+  //         {   nombre: jugador.nombre, 
+  //             _1r_apellido: jugador._1r_apellido,
+  //             _2n_apellido: jugador._2n_apellido,
+  //             curso: jugador.curso,
+  //             genero: jugador.genero_extra,
+  //             pertenece_equipo: Equipo_id,
+  //             email: jugador.email,
+  //             ficha: 'jugador',
               
-            },
-        ]).select()
+  //           },
+  //       ]).select()
   
-      if (jugadorError) {
-        console.error("Error insertando jugador principal:", jugadorError.message);
-        // Considera si quieres detener todo el proceso o continuar con los siguientes jugadores
-      }
-    } else {
-      let publicIMGurl = "";
-      // Llama a la función para subir el escudo
-    const JugadorIMGPath = await uploadJugadorIMG(jugador.img, jugador.email);
+  //     if (jugadorError) {
+  //       console.error("Error insertando jugador principal:", jugadorError.message);
+  //       // Considera si quieres detener todo el proceso o continuar con los siguientes jugadores
+  //     }
+  //   } else {
+  //     let publicIMGurl = "";
+  //     // Llama a la función para subir el escudo
+  //   const JugadorIMGPath = await uploadJugadorIMG(jugador.img, jugador.email);
     
-    //Obtener la URL pública del escudo subido
-    const { data: urlIMGData } = supabaseAdmin.storage
-        .from('JugadoresIMG')
-        .getPublicUrl(JugadorIMGPath); // Usa el escudoPath que se generó al subir el archivo
+  //   //Obtener la URL pública del escudo subido
+  //   const { data: urlIMGData } = supabaseAdmin.storage
+  //       .from('JugadoresIMG')
+  //       .getPublicUrl(JugadorIMGPath); // Usa el escudoPath que se generó al subir el archivo
     
-    // Verifica si urlData contiene la propiedad publicUrl
-    if (urlIMGData) {
-      publicIMGurl = urlIMGData.publicUrl;
-    }
+  //   // Verifica si urlData contiene la propiedad publicUrl
+  //   if (urlIMGData) {
+  //     publicIMGurl = urlIMGData.publicUrl;
+  //   }
   
-      const { error: jugadorError } = await supabaseAdmin
-        .from(TablaJugadores)
-        .insert([
-          {   nombre: jugador.nombre, 
-              _1r_apellido: jugador._1r_apellido,
-              _2n_apellido: jugador._2n_apellido,
-              curso: jugador.curso,
-              genero: jugador.genero_extra,
-              pertenece_equipo: Equipo_id,
-              email: jugador.email,
-              ficha: 'jugador',
-              img: publicIMGurl,
-            },
-        ]).select()
+  //     const { error: jugadorError } = await supabaseAdmin
+  //       .from(TablaJugadores)
+  //       .insert([
+  //         {   nombre: jugador.nombre, 
+  //             _1r_apellido: jugador._1r_apellido,
+  //             _2n_apellido: jugador._2n_apellido,
+  //             curso: jugador.curso,
+  //             genero: jugador.genero_extra,
+  //             pertenece_equipo: Equipo_id,
+  //             email: jugador.email,
+  //             ficha: 'jugador',
+  //             img: publicIMGurl,
+  //           },
+  //       ]).select()
   
-      if (jugadorError) {
-        console.error("Error insertando jugador principal:", jugadorError.message);
-        // Considera si quieres detener todo el proceso o continuar con los siguientes jugadores
-      }
-    }
-  }
+  //     if (jugadorError) {
+  //       console.error("Error insertando jugador principal:", jugadorError.message);
+  //       // Considera si quieres detener todo el proceso o continuar con los siguientes jugadores
+  //     }
+  //   }
+  // }
 
           
 
