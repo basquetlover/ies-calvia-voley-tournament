b3ad299f2835c70d96de47bf1f48b559c11b438a
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-12-14 20:27:20 +0100

    Update base-competicion.ts

--- src/const/base-competicion.ts
+++ src/const/base-competicion.ts
@@ -3,7 +3,7 @@ export const BASES_COMPETICION = [
         titulo: '1. Sistema de Puntuació',
         articulos:[
             {
-                texto: 'Es juga a dos sets. L’equip que guanyi dos sets primer, guanya el partit.',
+                texto: 'Es juga al millor de dos sets guanyats. L’equip que guanyi dos sets primer, guanya el partit.',
             },
             {
                 texto: 'Cada set es juga a 10 punts, sense diferència de dos punts.',
@@ -12,7 +12,7 @@ export const BASES_COMPETICION = [
                 texto: 'En cas d’empat a un set, el set decisiu es juga a 7 punts (sense diferència de dos punts).',
             },
             {
-                texto: 'A l’arribar als 20 minuts de partit i si els equips estan empatats a 0, es donaran 5 minuts de desempat; en cas de no finalitzar-lo, guanyarà l’equip amb més punts en el partit.',
+                texto: 'En arribar als 20 minuts de durada del partit i si els equips estan empatats a 0 sets, es disputaran 5 minuts de desempat; si aquest no finalitza, guanyarà l’equip amb més punts totals acumulats.',
             }
         ]
     },
@@ -51,10 +51,10 @@ export const BASES_COMPETICION = [
                 texto: 'Un jugador no pot tocar la pilota dues vegades consecutives (excepte en bloquejos).',
             },
             {
-                texto: 'Efectuar el toc de "Poqui" està permès',
+                texto: 'Efectuar el toc de "poqui" està permès.',
             },
             {
-                texto: 'Executar el moviment de pales està permès',
+                texto: 'Executar el moviment de pales està permès.',
             }
         ]
     },
@@ -73,19 +73,19 @@ export const BASES_COMPETICION = [
         titulo: '6. Faltes',
         articulos:[
             {
-                texto: 'Pilota fora: La pilota surt dels límits del camp.',
+                texto: 'Pilota fora: la pilota surt dels límits del camp.',
             },
             {
-                texto: 'Quatre tocs: L’equip fa més de tres tocs abans de tornar la pilota.',
+                texto: 'Quatre tocs: l’equip fa més de tres tocs abans de tornar la pilota.',
             },
             {
-                texto: 'Retenció o doble toc: El jugador sosté o toca la pilota dues vegades consecutives.',
+                texto: 'Retenció o doble toc: el jugador sosté o toca la pilota dues vegades consecutives.',
             },
             {
-                texto: 'Invasió: Un jugador toca la xarxa o creua la línia central.',
+                texto: 'Invasió: un jugador toca la xarxa o creua la línia central.',
             },
             {
-                texto: 'Contacte amb l’entorn: Si la pilota toca el sostre, les parets o qualsevol element de bàsquet (com cistelles o cèrcols) dins del pavelló, serà punt per a l’equip rival.',
+                texto: 'Contacte amb l’entorn: si la pilota toca el sostre, les parets, les cistelles, els cèrcols o qualsevol element del pavelló, serà punt per a l’equip rival.',
             }
         ]
     },
@@ -104,13 +104,13 @@ export const BASES_COMPETICION = [
         titulo: '8. Arbitratge',
         articulos:[
             {
-                texto: 'Cada partit comptarà amb un àrbitre principal i, si és possible, un àrbitre assistent o jutges de línia.',
+                texto: 'Cada partit comptarà amb un àrbitre principal i, si és possible, amb un àrbitre assistent o jutges de línia.',
             },
             {
                 texto: 'L’àrbitre té autoritat per sancionar faltes, validar punts i aplicar penalitzacions si fos necessari.',
             },
             {
-                texto: 'Les decisions de l’àrbitre són finals i s’han de respectar per garantir el flux del joc.',
+                texto: 'Les decisions de l’àrbitre són definitives i s’han de respectar.',
             }
         ]
     },
@@ -118,10 +118,10 @@ export const BASES_COMPETICION = [
         titulo: '9. Protocol d’Inici de Partit',
         articulos:[
             {
-                texto: 'Abans de cada partit, els capitans de tots dos equips es reuniran amb l’àrbitre per llançar una moneda i decidir qui realitzarà el primer servei.',
+                texto: 'Abans de cada partit, els capitans dels dos equips es reuniran amb l’àrbitre per fer un sorteig i decidir el primer servei.',
             },
             {
-                texto: 'L’equip que guanyi el sorteig decideix si vol servir o rebre.',
+                texto: 'L’equip que guanyi el sorteig decidirà si vol servir o rebre.',
             }
         ]
     },
@@ -129,10 +129,10 @@ export const BASES_COMPETICION = [
         titulo: '10. Regles de Conducta',
         articulos:[
             {
-                texto: 'Els jugadors, entrenadors i acompanyants han de respectar els àrbitres i els oponents en tot moment.',
+                texto: 'Els jugadors, entrenadors i acompanyants han de respectar els àrbitres i els rivals en tot moment.',
             },
             {
-                texto: 'Qualsevol actitud antiesportiva, com insults o provocacions, serà sancionada amb advertència, i en casos greus, amb l’expulsió del jugador o de l’equip del partit o del torneig.',
+                texto: 'Qualsevol actitud antiesportiva serà sancionada amb advertència i, en casos greus, amb l’expulsió del jugador o de l’equip del partit o del torneig.',
             }
         ]
     },
@@ -140,10 +140,95 @@ export const BASES_COMPETICION = [
         titulo: '11. Protocols de Seguretat',
         articulos:[
             {
-                texto: 'Es recomana que tots els jugadors facin servir calçat adequat i mantinguin l’àrea de joc lliure d’objectes que puguin causar ensopegades.',
+                texto: 'Es recomana que tots els jugadors utilitzin calçat adequat i mantinguin l’àrea de joc lliure d’objectes perillosos.',
             },
             {
                 texto: 'En cas de lesió, el joc es detindrà temporalment fins que el jugador sigui atès.',
+            },
+            {
+                texto: 'Si un equip no pot continuar el partit per lesió, aquest es donarà per perdut.',
+            }
+        ]
+    },
+    { 
+        titulo: '12. Composició Mixta Obligatòria',
+        articulos:[
+            {
+                texto: 'Durant tot el partit, cada equip ha de tenir com a mínim una persona de cada gènere en pista.',
+            },
+            {
+                texto: 'Si un equip incompleix aquesta norma en qualsevol moment, el partit es donarà per perdut automàticament.',
+            },
+            {
+                texto: 'Tot i això, el partit es continuarà jugant fins a completar els dos sets, independentment del resultat.',
+            },
+            {
+                texto: 'A efectes de classificació, el partit es considerarà guanyat per l’altre equip.',
+            }
+        ]
+    },
+    { 
+        titulo: '13. Protocol d’Escalfament i Ús del Material',
+        articulos:[
+            {
+                texto: 'Cada equip tindrà una pista d’escalfament assignada i no està permès canviar-la.',
+            },
+            {
+                texto: 'Cada equip disposarà d’una pilota pròpia per a l’escalfament.',
+            },
+            {
+                texto: 'Si una pilota entra a una pista de joc, es farà un primer avís; a la segona vegada, la pilota serà retirada durant aquella ronda d’escalfament.',
+            },
+            {
+                texto: 'El mal ús del material comportarà la prohibició d’utilitzar material d’escalfament durant la resta del torneig.',
+            }
+        ]
+    },
+    { 
+        titulo: '14. Sistema de Competició i Classificació',
+        articulos:[
+            {
+                texto: 'El torneig compta amb 16 equips i es disputa mitjançant un quadre d’eliminació directa des dels vuitens de final fins a la final.',
+            },
+            {
+                texto: 'No es disputa partit pel tercer i quart lloc.',
+            },
+            {
+                texto: 'Tots els equips disputaran un mínim de tres partits.',
+            },
+            {
+                texto: 'Els equips que perdin jugaran partits de reubicació segons el balanç de partits guanyats i perduts per igualar el nivell competitiu.',
+            },
+            {
+                texto: 'Les victòries o derrotes administratives comptaran a tots els efectes per al balanç del torneig.',
+            }
+        ]
+    },
+    { 
+        titulo: '15. Gestió del Temps i Jornades de Joc',
+        articulos:[
+            {
+                texto: 'El torneig es desenvolupa en 12 jornades de 20 minuts cadascuna.',
+            },
+            {
+                texto: 'Els partits han de començar immediatament a l’inici de la jornada assignada.',
+            },
+            {
+                texto: 'Si un partit no ha començat, l’organització podrà fer avisos acústics per indicar l’inici immediat.',
+            },
+            {
+                texto: 'Un equip que no estigui preparat o no es presenti podrà perdre el partit per decisió de l’organització.',
+            }
+        ]
+    },
+    { 
+        titulo: '16. Validació de Resultats',
+        articulos:[
+            {
+                texto: 'El resultat validat per l’àrbitre i registrat a l’acta serà definitiu.',
+            },
+            {
+                texto: 'Un cop validat el resultat, els encreuaments següents no es modificaran.',
             }
         ]
     }
