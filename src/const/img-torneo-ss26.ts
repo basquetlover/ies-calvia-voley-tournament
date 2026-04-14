f7e1efab4e150e6777bee99d91104215bacdaad3
Pere Alemany <85557755+basquetlover@users.noreply.github.com>
2026-04-14 10:53:36 +0200

    actualizar galeria

---
+++ src/const/img-torneo-ss26.ts
@@ -0,0 +1,551 @@
+export const IMG = [
+
+{
+        referencia: "00026-191.jpg"
+},
+{
+    referencia: "00026-192.jpg"
+},
+{
+    referencia: "00026-193.jpg"
+},
+{
+    referencia: "00026-194.jpg"
+},
+{
+    referencia: "00026-195.jpg"
+},
+{
+    referencia: "00026-196.jpg"
+},
+{
+    referencia: "00026-197.jpg"
+},
+{
+    referencia: "00026-198.jpg"
+},
+{
+    referencia: "00026-199.jpg"
+},
+{
+    referencia: "00026-200.jpg"
+},
+{
+    referencia: "00026-201.jpg"
+},
+{
+    referencia: "00026-202.jpg"
+},
+{
+    referencia: "00026-203.jpg"
+},
+{
+    referencia: "00026-204.jpg"
+},
+{
+    referencia: "00026-205.jpg"
+},
+{
+    referencia: "00026.jpg"
+},
+{
+    referencia: "00026-2.jpg"
+},
+{
+    referencia: "00026-3.jpg"
+},
+{
+    referencia: "00026-4.jpg"
+},
+{
+    referencia: "00026-5.jpg"
+},
+{
+    referencia: "00026-6.jpg"
+},
+{
+    referencia: "00026-7.jpg"
+},
+{
+    referencia: "00026-8.jpg"
+},
+{
+    referencia: "00026-10.jpg"
+},
+{
+    referencia: "00026-11.jpg"
+},
+{
+    referencia: "00026-12.jpg"
+},
+{
+    referencia: "00026-13.jpg"
+},
+{
+    referencia: "00026-14.jpg"
+},
+{
+    referencia: "00026-15.jpg"
+},
+{
+    referencia: "00026-16.jpg"
+},
+{
+    referencia: "00026-18.jpg"
+},
+{
+    referencia: "00026-19.jpg"
+},
+{
+    referencia: "00026-21.jpg"
+},
+{
+    referencia: "00026-22.jpg"
+},
+{
+    referencia: "00026-23.jpg"
+},
+{
+    referencia: "00026-24.jpg"
+},
+{
+    referencia: "00026-25.jpg"
+},
+{
+    referencia: "00026-26.jpg"
+},
+{
+    referencia: "00026-27.jpg"
+},
+{
+    referencia: "00026-28.jpg"
+},
+{
+    referencia: "00026-29.jpg"
+},
+{
+    referencia: "00026-30.jpg"
+},
+{
+    referencia: "00026-31.jpg"
+},
+{
+    referencia: "00026-32.jpg"
+},
+{
+    referencia: "00026-33.jpg"
+},
+{
+    referencia: "00026-34.jpg"
+},
+{
+    referencia: "00026-36.jpg"
+},
+{
+    referencia: "00026-37.jpg"
+},
+{
+    referencia: "00026-38.jpg"
+},
+{
+    referencia: "00026-39.jpg"
+},
+{
+    referencia: "00026-40.jpg"
+},
+{
+    referencia: "00026-41.jpg"
+},
+{
+    referencia: "00026-42.jpg"
+},
+{
+    referencia: "00026-43.jpg"
+},
+{
+    referencia: "00026-44.jpg"
+},
+{
+    referencia: "00026-45.jpg"
+},
+{
+    referencia: "00026-47.jpg"
+},
+{
+    referencia: "00026-48.jpg"
+},
+{
+    referencia: "00026-50.jpg"
+},
+{
+    referencia: "00026-51.jpg"
+},
+{
+    referencia: "00026-52.jpg"
+},
+{
+    referencia: "00026-53.jpg"
+},
+{
+    referencia: "00026-55.jpg"
+},
+{
+    referencia: "00026-56.jpg"
+},
+{
+    referencia: "00026-57.jpg"
+},
+{
+    referencia: "00026-58.jpg"
+},
+{
+    referencia: "00026-59.jpg"
+},
+{
+    referencia: "00026-60.jpg"
+},
+{
+    referencia: "00026-61.jpg"
+},
+{
+    referencia: "00026-63.jpg"
+},
+{
+    referencia: "00026-64.jpg"
+},
+{
+    referencia: "00026-65.jpg"
+},
+{
+    referencia: "00026-66.jpg"
+},
+{
+    referencia: "00026-67.jpg"
+},
+{
+    referencia: "00026-68.jpg"
+},
+{
+    referencia: "00026-70.jpg"
+},
+{
+    referencia: "00026-71.jpg"
+},
+{
+    referencia: "00026-75.jpg"
+},
+{
+    referencia: "00026-76.jpg"
+},
+{
+    referencia: "00026-78.jpg"
+},
+{
+    referencia: "00026-79.jpg"
+},
+{
+    referencia: "00026-80.jpg"
+},
+{
+    referencia: "00026-81.jpg"
+},
+{
+    referencia: "00026-82.jpg"
+},
+{
+    referencia: "00026-84.jpg"
+},
+{
+    referencia: "00026-85.jpg"
+},
+{
+    referencia: "00026-86.jpg"
+},
+{
+    referencia: "00026-87.jpg"
+},
+{
+    referencia: "00026-88.jpg"
+},
+{
+    referencia: "00026-89.jpg"
+},
+{
+    referencia: "00026-90.jpg"
+},
+{
+    referencia: "00026-91.jpg"
+},
+{
+    referencia: "00026-92.jpg"
+},
+{
+    referencia: "00026-93.jpg"
+},
+{
+    referencia: "00026-94.jpg"
+},
+{
+    referencia: "00026-95.jpg"
+},
+{
+    referencia: "00026-96.jpg"
+},
+{
+    referencia: "00026-97.jpg"
+},
+{
+    referencia: "00026-98.jpg"
+},
+{
+    referencia: "00026-99.jpg"
+},
+{
+    referencia: "00026-100.jpg"
+},
+{
+    referencia: "00026-101.jpg"
+},
+{
+    referencia: "00026-102.jpg"
+},
+{
+    referencia: "00026-103.jpg"
+},
+{
+    referencia: "00026-104.jpg"
+},
+{
+    referencia: "00026-105.jpg"
+},
+{
+    referencia: "00026-106.jpg"
+},
+{
+    referencia: "00026-107.jpg"
+},
+{
+    referencia: "00026-108.jpg"
+},
+{
+    referencia: "00026-109.jpg"
+},
+{
+    referencia: "00026-110.jpg"
+},
+{
+    referencia: "00026-111.jpg"
+},
+{
+    referencia: "00026-112.jpg"
+},
+{
+    referencia: "00026-113.jpg"
+},
+{
+    referencia: "00026-114.jpg"
+},
+{
+    referencia: "00026-115.jpg"
+},
+{
+    referencia: "00026-116.jpg"
+},
+{
+    referencia: "00026-117.jpg"
+},
+{
+    referencia: "00026-118.jpg"
+},
+{
+    referencia: "00026-119.jpg"
+},
+{
+    referencia: "00026-120.jpg"
+},
+{
+    referencia: "00026-121.jpg"
+},
+{
+    referencia: "00026-122.jpg"
+},
+{
+    referencia: "00026-123.jpg"
+},
+{
+    referencia: "00026-124.jpg"
+},
+{
+    referencia: "00026-126.jpg"
+},
+{
+    referencia: "00026-127.jpg"
+},
+{
+    referencia: "00026-128.jpg"
+},
+{
+    referencia: "00026-129.jpg"
+},
+{
+    referencia: "00026-130.jpg"
+},
+{
+    referencia: "00026-131.jpg"
+},
+{
+    referencia: "00026-133.jpg"
+},
+{
+    referencia: "00026-134.jpg"
+},
+{
+    referencia: "00026-135.jpg"
+},
+{
+    referencia: "00026-136.jpg"
+},
+{
+    referencia: "00026-137.jpg"
+},
+{
+    referencia: "00026-138.jpg"
+},
+{
+    referencia: "00026-139.jpg"
+},
+{
+    referencia: "00026-140.jpg"
+},
+{
+    referencia: "00026-141.jpg"
+},
+{
+    referencia: "00026-142.jpg"
+},
+{
+    referencia: "00026-143.jpg"
+},
+{
+    referencia: "00026-144.jpg"
+},
+{
+    referencia: "00026-145.jpg"
+},
+{
+    referencia: "00026-146.jpg"
+},
+{
+    referencia: "00026-147.jpg"
+},
+{
+    referencia: "00026-148.jpg"
+},
+{
+    referencia: "00026-149.jpg"
+},
+{
+    referencia: "00026-150.jpg"
+},
+{
+    referencia: "00026-152.jpg"
+},
+{
+    referencia: "00026-153.jpg"
+},
+{
+    referencia: "00026-154.jpg"
+},
+{
+    referencia: "00026-155.jpg"
+},
+{
+    referencia: "00026-156.jpg"
+},
+{
+    referencia: "00026-157.jpg"
+},
+{
+    referencia: "00026-159.jpg"
+},
+{
+    referencia: "00026-160.jpg"
+},
+{
+    referencia: "00026-161.jpg"
+},
+{
+    referencia: "00026-162.jpg"
+},
+{
+    referencia: "00026-163.jpg"
+},
+{
+    referencia: "00026-164.jpg"
+},
+{
+    referencia: "00026-165.jpg"
+},
+{
+    referencia: "00026-166.jpg"
+},
+{
+    referencia: "00026-169.jpg"
+},
+{
+    referencia: "00026-172.jpg"
+},
+{
+    referencia: "00026-173.jpg"
+},
+{
+    referencia: "00026-174.jpg"
+},
+{
+    referencia: "00026-175.jpg"
+},
+{
+    referencia: "00026-176.jpg"
+},
+{
+    referencia: "00026-177.jpg"
+},
+{
+    referencia: "00026-178.jpg"
+},
+{
+    referencia: "00026-179.jpg"
+},
+{
+    referencia: "00026-180.jpg"
+},
+{
+    referencia: "00026-181.jpg"
+},
+{
+    referencia: "00026-182.jpg"
+},
+{
+    referencia: "00026-183.jpg"
+},
+{
+    referencia: "00026-184.jpg"
+},
+{
+    referencia: "00026-185.jpg"
+},
+{
+    referencia: "00026-186.jpg"
+},
+{
+    referencia: "00026-187.jpg"
+},
+{
+    referencia: "00026-188.jpg"
+},
+{
+    referencia: "00026-189.jpg"
+},
+{
+    referencia: "00026-190.jpg"
+}]
