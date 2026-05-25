7bb3f7756df3f7cec715e93ab7c15cfcad3247b7
basquetlover <85557755+basquetlover@users.noreply.github.com>
2026-05-25 12:12:26 +0200

    Update PreviewVoluntari.tsx

--- src/components/panel/voluntaris/PreviewVoluntari.tsx
+++ src/components/panel/voluntaris/PreviewVoluntari.tsx
@@ -158,6 +158,21 @@ export default function PreviewVoluntari({ torneoID, voluntariID }: Props) {
                                         <p>Observacions</p>
                                         <p className="w-full min-h-20 px-2 py-1 bg-gris/40 rounded border border-gray-600">{data.observacion}</p>
                                     </div>
+
+                                    <div className="w-full grid grid-cols-2 gap-5">
+                                        <div className="w-full h-16 cursor-pointer rounded px-3 bg-primary text-azul-suave fill-azul-suave flex hover:bg-accent duration-300 items-center place-content-center gap-x-2 font-medium">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 -960 960 960">
+                                                <path d="m424-296 282-282-56-56-226 226-114-114-56 56zm56 216q-83 0-156-31.5T197-197t-85.5-127T80-480t31.5-156T197-763t127-85.5T480-880t156 31.5T763-763t85.5 127T880-480t-31.5 156T763-197t-127 85.5T480-80m0-80q134 0 227-93t93-227-93-227-227-93-227 93-93 227 93 227 227 93m0-320"/>
+                                            </svg>
+                                            <p className="uppercase text-center">Acceptar Voluntari</p>
+                                        </div>
+                                        <div className="w-full h-16 cursor-pointer rounded px-3 flex hover:bg-red-600/30 duration-300 border border-red-400 text-red-400 fill-red-400 items-center place-content-center gap-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 -960 960 960">
+                                                <path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144zM480-80q-83 0-156-31.5T197-197t-85.5-127T80-480t31.5-156T197-763t127-85.5T480-880t156 31.5T763-763t85.5 127T880-480t-31.5 156T763-197t-127 85.5T480-80m0-80q134 0 227-93t93-227-93-227-227-93-227 93-93 227 93 227 227 93m0-320"/>
+                                            </svg>
+                                            <p className="uppercase text-center">Denegar Voluntari</p>
+                                        </div>
+                                    </div>
                                     
                             </div>
                             </>
